c1b450f4be9e6168eee78a0b0575d9568554d3b0
Cesar <cesargalansanchez06@gmail.com>
2026-03-10 11:00:51 +0100

    fix: exclude mcp/* from CSRF protection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            'mcp/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
